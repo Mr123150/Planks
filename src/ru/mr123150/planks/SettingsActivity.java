300cca84c2922a5a2802746c9458fb8240ba9134
Viktor Snesarevskiy <snesarevskiy@yandex.ru>
2014-09-26 01:26:19 +0400

    added button functions to settings

--- src/ru/mr123150/planks/SettingsActivity.java
+++ src/ru/mr123150/planks/SettingsActivity.java
@@ -1,20 +1,32 @@
 package ru.mr123150.planks;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 
-	EditText breakTime;
-	EditText plankTime[] = new EditText[6];
+	EditText breakTimeText;
+	EditText plankTimeText[] = new EditText[6];
 	
+	int breakTime;
+	int plankTime[] = new int[6];
+	
+	public static final String APP_PREFERENCES = "settings";
+	public static final String APP_PREFERENCES_TIMER_0 = "Timer";
+	public static final String APP_PREFERENCES_TIMER_1 = "Timer";
+	public static final String APP_PREFERENCES_TIMER_2 = "Timer";
+	public static final String APP_PREFERENCES_TIMER_3 = "Timer";
+	public static final String APP_PREFERENCES_TIMER_4 = "Timer";
+	public static final String APP_PREFERENCES_TIMER_5 = "Timer";
+	public static final String APP_PREFERENCES_TIMER_BREAK = "TimerBreak";
+	
+	SharedPreferences settings;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,48 +35,100 @@ public class SettingsActivity extends Activity {
 		// Show the Up button in the action bar.
 		setupActionBar();
 		
-		breakTime=(EditText)findViewById(R.id.breakTime);
+		breakTimeText=(EditText)findViewById(R.id.breakTime);
+		plankTimeText[0]=(EditText)findViewById(R.id.plankTime0);
+		plankTimeText[1]=(EditText)findViewById(R.id.plankTime1);
+		plankTimeText[2]=(EditText)findViewById(R.id.plankTime2);
+		plankTimeText[3]=(EditText)findViewById(R.id.plankTime3);
+		plankTimeText[4]=(EditText)findViewById(R.id.plankTime4);
+		plankTimeText[5]=(EditText)findViewById(R.id.plankTime5);
+		
+		settings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+		plankTime[0]=settings.getInt(APP_PREFERENCES_TIMER_0, 30);
+		plankTime[1]=settings.getInt(APP_PREFERENCES_TIMER_1, 30);
+		plankTime[2]=settings.getInt(APP_PREFERENCES_TIMER_2, 30);
+		plankTime[3]=settings.getInt(APP_PREFERENCES_TIMER_3, 30);
+		plankTime[4]=settings.getInt(APP_PREFERENCES_TIMER_4, 30);
+		plankTime[5]=settings.getInt(APP_PREFERENCES_TIMER_5, 30);
+		breakTime=settings.getInt(APP_PREFERENCES_TIMER_BREAK, 5);
+		
+		breakTimeText.setText(Integer.toString(breakTime));
+		for(int i=0;i<6;++i){
+			plankTimeText[i].setText(Integer.toString(plankTime[i]));
+		}
+		
 	}
 	
 	public void resetClick(View v){
-		
+		breakTimeText.setText("5");
+		for(int i=0;i<6;++i){
+			plankTimeText[i].setText("30");
+		}
 	}
 	
 	public void breakMinusClick(View v){
-		
+		int time=Integer.parseInt(breakTimeText.getText().toString());
+		if(time<1)time=1;
+		breakTimeText.setText(Integer.toString(time-1));
 	}
 	
 	public void breakPlusClick(View v){
-		
+		int time=Integer.parseInt(breakTimeText.getText().toString());
+		breakTimeText.setText(Integer.toString(time+1));
 	}
 	
 	public void plankMinusClick(View v){
-		
+		int i=0;
+		switch(v.getId()){
+		case R.id.plankPlus0:
+			i=0;
+			break;
+		case R.id.plankPlus1:
+			i=1;
+			break;
+		case R.id.plankPlus2:
+			i=2;
+			break;
+		case R.id.plankPlus3:
+			i=3;
+			break;
+		case R.id.plankPlus4:
+			i=4;
+			break;
+		case R.id.plankPlus5:
+			i=5;
+			break;
+		}
+		int time=Integer.parseInt(plankTimeText[i].getText().toString());
+		if(time<5)time=5;
+		plankTimeText[i].setText(Integer.toString(time-5));
 	}
 	
 	public void plankPlusClick(View v){
+		int i=0;
 		switch(v.getId()){
 		case R.id.plankPlus0:
-			Toast.makeText(this, "0", Toast.LENGTH_SHORT).show();
+			i=0;
 			break;
 		case R.id.plankPlus1:
-			Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+			i=1;
 			break;
 		case R.id.plankPlus2:
-			Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+			i=2;
 			break;
 		case R.id.plankPlus3:
-			Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
+			i=3;
 			break;
 		case R.id.plankPlus4:
-			Toast.makeText(this, "4", Toast.LENGTH_SHORT).show();
+			i=4;
 			break;
 		case R.id.plankPlus5:
-			Toast.makeText(this, "5", Toast.LENGTH_SHORT).show();
+			i=5;
 			break;
-		default:
-			Toast.makeText(this, "unknown", Toast.LENGTH_SHORT).show();
 		}
+		int time=Integer.parseInt(plankTimeText[i].getText().toString());
+		plankTimeText[i].setText(Integer.toString(time+5));
 	}
 
 	/**
@@ -76,12 +140,12 @@ public class SettingsActivity extends Activity {
 
 	}
 
-	@Override
+	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
-	}
+	}*/
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
