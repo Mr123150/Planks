fa93c4124ec2c907fdef7c8d5c60ed7fa4423cf1
Viktor Snesarevskiy <snesarevskiy@yandex.ru>
2014-09-26 00:58:56 +0400

    finished settings layout

--- src/ru/mr123150/planks/SettingsActivity.java
+++ src/ru/mr123150/planks/SettingsActivity.java
@@ -1,19 +1,70 @@
 package ru.mr123150.planks;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 
+	EditText breakTime;
+	EditText plankTime[] = new EditText[6];
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		breakTime=(EditText)findViewById(R.id.breakTime);
+	}
+	
+	public void resetClick(View v){
+		
+	}
+	
+	public void breakMinusClick(View v){
+		
+	}
+	
+	public void breakPlusClick(View v){
+		
+	}
+	
+	public void plankMinusClick(View v){
+		
+	}
+	
+	public void plankPlusClick(View v){
+		switch(v.getId()){
+		case R.id.plankPlus0:
+			Toast.makeText(this, "0", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.plankPlus1:
+			Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.plankPlus2:
+			Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.plankPlus3:
+			Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.plankPlus4:
+			Toast.makeText(this, "4", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.plankPlus5:
+			Toast.makeText(this, "5", Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			Toast.makeText(this, "unknown", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	/**
@@ -36,13 +87,6 @@ public class SettingsActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
